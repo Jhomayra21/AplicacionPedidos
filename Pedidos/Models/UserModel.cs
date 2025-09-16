58489e4e4d368b42adee423ad0cf5e5dedad223e
Jhomayra21 <jhomayramamanichoqueticlla21@gmail.com>
2025-09-16 19:29:03 -0400

    creacion_modelos

---
+++ Pedidos/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Pedidos.Models
+{
+    public class UserModel
+    {
+        public string nombre { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string rol { get; set; }
+    }
+}
